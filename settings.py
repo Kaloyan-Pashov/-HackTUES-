2cfc052e37c2a0567d06700a948303262b64b26a
Kaloyan Pashov <kaloyan1709@gmail.com>
2019-03-15 15:33:55 +0200

    Settings_Window_Update 1
    Added some notification buttons and rearranged the other buttons.

--- settings.py
+++ settings.py
@@ -2,13 +2,21 @@ from tkinter import *
 
 window = Tk()
 window.title("GUI")
-window.geometry('200x100')
+window.geometry('400x200')
+
+bored_eye = BooleanVar()
+act_noti = BooleanVar()
+dist_bool = BooleanVar()
+
+Label(window, text = "Settings:", fg = "black", width = 20).pack()
+Label(window, text="Notifications:").pack(anchor='w')
+
+Checkbutton(window, text="Bored Eye notifications", variable = bored_eye).pack()
+Checkbutton(window, text = " Activity notifications   ", variable = act_noti).pack()
+Checkbutton(window, text = "Distance from the monitor notifications", variable = dist_bool).pack()
 
 
 
-Button(window, text = "Settings", fg = "black").pack(fill = "x")
 
-
-Button(window, text = "START", fg = "black").pack(fill = "x")
-
+Button(window, text = "START", fg = "black").pack(side = 'right', fill = "x")
 window.mainloop()
