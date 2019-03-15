4a6ea0e44f13a2605088c850dcf0cc1088bf7535
Kaloyan Pashov <pashov.kaloyan@gmail.com>
2019-03-15 18:29:03 +0200

    Settings_Window_Update 2
    Added more settings.

--- settings.py
+++ settings.py
@@ -1,22 +1,46 @@
 from tkinter import *
+from tkinter import simpledialog
+import time
 
 window = Tk()
 window.title("GUI")
 window.geometry('400x200')
+window.resizable(width = False, height = False)
 
-bored_eye = BooleanVar()
-act_noti = BooleanVar()
-dist_bool = BooleanVar()
+bored_eye = BooleanVar(value = False)
+act_noti = BooleanVar(value = False)
+dist_bool = BooleanVar(value = False)
+bored_eye_time = None
+act_noti_time = None
+dist_bool_time = None
+
 
 Label(window, text = "Settings:", fg = "black", width = 20).pack()
 Label(window, text="Notifications:").pack(anchor='w')
-
 Checkbutton(window, text="Bored Eye notifications", variable = bored_eye).pack()
 Checkbutton(window, text = " Activity notifications   ", variable = act_noti).pack()
 Checkbutton(window, text = "Distance from the monitor notifications", variable = dist_bool).pack()
 
 
 
+Button(window, text = "START", fg = "black", bg = 'blue').pack(side = 'right', fill = 'x')
 
-Button(window, text = "START", fg = "black").pack(side = 'right', fill = "x")
+while 1:
+    if bored_eye.get() == True and not bored_eye_time:
+        bored_eye_time = simpledialog.askinteger("", "How much time between notifications? (in minutes)")
+        
+    if act_noti.get() == True and not act_noti_time:
+        act_noti_time =  simpledialog.askinteger("", "How much time between notifications? (in minutes)")
+        
+    if dist_bool.get() == True and not dist_bool_time:
+        dist_bool_time = simpledialog.askinteger("", "How much time between notifications? (in minutes)") 
+    window.update()
+    time.sleep(0.01)
+    
+
+
+
+
+
 window.mainloop()
+
