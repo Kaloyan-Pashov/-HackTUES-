0ea8dd2785320b852917083c0a079897165d27a1
dzhelek <38794411+dzhelek@users.noreply.github.com>
2019-03-16 10:20:02 +0200

    Merge
    Merge writefile.py and settings.py.

--- src/python/settings.py
+++ src/python/settings.py
@@ -4,40 +4,64 @@ import time
 
 window = Tk()
 window.title("GUI")
-window.geometry('400x200')
+window.geometry('400x275')
 window.resizable(width=False, height=False)
+sets = {}
 
 
 class Setting:
     def __init__(self, text, master):
         self.var = BooleanVar(value=None)
-        self.time = None
+        self.time = 0
         self.text = text
         self.master = master
         Checkbutton(self.master, text=self.text, variable=self.var).pack()
+        sets[self.text] = self.time
 
     def ask(self):
         if self.var.get() and not self.time:
             self.time = simpledialog.askinteger("ask", "How much time between " + self.text + "? (in minutes)")
+            sets[self.text] = self.time
         if not self.var.get():
-            self.time = None
+            self.time = 0
+
+
+def shut_down():
+    window.destroy()
+    f = open("settings.txt", 'w')
+
+    for x in sets.keys():
+        f.write(x + '\n')
+        f.write(str(sets[x]) + '\n')
 
 
 Label(window, text="Settings:", fg="black", width=20).pack()
 Label(window, text="Notifications:").pack(anchor='w')
-bored_eye = Setting("Bored Eye notifications", window)
-act_noti = Setting("Activity notifications", window)
-dist_bool = Setting("Distance from the monitor notifications", window)
-settings = [bored_eye, act_noti, dist_bool]
+bored_eye = Setting("bored eye notifications", window)
+act_noti = Setting("activity notifications", window)
+dist_bool = Setting("distance from the monitor notifications", window)
+drink_w = Setting("drink water reminders", window)
+lunch_br = Setting("remind before lunchbreak", window)
+post_r = Setting("remind me to imrove posture", window)
+gym_r = Setting("remind me to do gymnastics", window)
+end_r = Setting("notify me when there are 30 minutes left of the work day", window)
+
+settings = [bored_eye, act_noti, dist_bool, drink_w, lunch_br, post_r, gym_r, end_r]
+
+Button(window, text="START", fg="black", command=shut_down).pack(side='right', fill='x')
 
 
-Button(window, text="START", fg="black", bg='blue').pack(side='right', fill='x')
+
 
 while 1:
     for i in settings:
         i.ask()
 
-    window.update()
+    try:
+        window.update()
+    except TclError:
+        pass
+
     time.sleep(0.01)
 
 window.mainloop()
--- src/python/writefile.py
+++
@@ -1,6 +0,0 @@
-sets = {'somesets': True, 'numbersets': 8,}
-f = open("settings.txt", 'w')
-
-for x in sets.keys():
-    f.write(x + '\n')
-    f.write(str(sets[x]) + '\n')
